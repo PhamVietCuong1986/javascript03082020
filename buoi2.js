2d3832454902559a9c214ff1f395bd5c57e30589
Nguyen van D <vietcuong1226@gmail.com>
2020-08-09 22:46:32 +0700

    ve interable mng Array khac voi Object

--- buoi2.js
+++ buoi2.js
@@ -9,9 +9,20 @@
 // for(var value of arrayNumbers){
 //     console.log(value)
 // }
+// for of doi object
+const teo = {
+    name : "nguyen van teo",
+    age : 10
+}
+for (const value of teo) {
+    console.log(value)
+    
+}
 // for in  doi voi Array 
 // truyen key cua mang
-const arrayNumbers = [5,4,10,15,6]
-for (const key in arrayNumbers) {
-    console.log(key)
-}
+// const arrayNumbers = [5,4,10,15,6]
+// for (const key in arrayNumbers) {
+//     console.log(key)
+//}
+// interabal manng tim lay du lieu thong qua index
+// object tim  vaf lay du lieu thong qua thuocj tinh cua bien
