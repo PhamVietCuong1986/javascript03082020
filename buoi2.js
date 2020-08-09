2bba1f35f30592723ed2cae97b6ea9b2cf523c4d
Nguyen van D <vietcuong1226@gmail.com>
2020-08-09 23:12:23 +0700

    for in cho object

--- buoi2.js
+++ buoi2.js
@@ -9,20 +9,21 @@
 // for(var value of arrayNumbers){
 //     console.log(value)
 // }
-// for of doi object
-const teo = {
-    name : "nguyen van teo",
-    age : 10
-}
-for (const value of teo) {
-    console.log(value)
-    
-}
+// for of khong su dung cho Object
+
 // for in  doi voi Array 
 // truyen key cua mang
 // const arrayNumbers = [5,4,10,15,6]
 // for (const key in arrayNumbers) {
 //     console.log(key)
 //}
+// for in tim va lay gia tri object
+const teo = {
+    name : "nguyen van teo",
+    age : 10
+}
+for (const key in teo) {
+    console.log(key)
+}
 // interabal manng tim lay du lieu thong qua index
-// object tim  vaf lay du lieu thong qua thuocj tinh cua bien
+// object tim  va lay du lieu thong qua thuoc tinh cua bien
