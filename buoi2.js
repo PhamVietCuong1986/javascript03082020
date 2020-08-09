0791409df4c0306c66038c378e1be2de889ba456
Nguyen van D <vietcuong1226@gmail.com>
2020-08-09 22:20:54 +0700

    for of, for in doi voi Array

---
+++ buoi2.js
@@ -0,0 +1,17 @@
+//9. vong lap for
+// for (var i = 0 ; i < 10 ; i++){
+//     console.log(i)
+// }
+//for of doi voi Array
+
+// lay gia tri trong mang du lieu
+// const arrayNumbers = [5,4,10,15,6]
+// for(var value of arrayNumbers){
+//     console.log(value)
+// }
+// for in  doi voi Array 
+// truyen key cua mang
+const arrayNumbers = [5,4,10,15,6]
+for (const key in arrayNumbers) {
+    console.log(key)
+}
