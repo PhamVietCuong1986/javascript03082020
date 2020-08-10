58cfcbf0a746d80f8ce85fbb50ea85daa9e305d5
Nguyen van D <vietcuong1226@gmail.com>
2020-08-10 11:39:02 +0700

    bai tap hay ve for

--- buoi2.js
+++ buoi2.js
@@ -28,10 +28,50 @@
 // interabal manng tim lay du lieu thong qua index
 // object tim  va lay du lieu thong qua thuoc tinh cua bien
 // thuc hanh bai tap
-var a = [1, 2, 4, 8, 16]
-// su dung vong lap for de lay gia tri tu cuoi tre ve
-for(var i = a.length - 1; i >= 0; i--){
-    console.log(a[i])
-}
+// var a = [1, 2, 4, 8, 16]
+// // su dung vong lap for de lay gia tri tu cuoi tre ve
+// for(var i = a.length - 1; i >= 0; i--){
+//     console.log(a[i])
+// }
+/**
+ * Sử dụng vòng lặp for...in để in ra tất cả các key trong object sau, in ra cả các key của các nested object (object con lồng bên trong object lớn)
+ * Gợi ý: đây là bài tập khó, bạn nên tìm hiểu về recursive trước
+ */
+
+var apartment = {
+    bedroom: {
+      area: 20,
+      bed: {
+        type: 'twin-bed',
+        price: 100
+      }
+    }
+  };
+  
+  function getkey(object){
+      for (const key in object) {
+          console.log(key)
+          for(const keyBedroom in object[key]){
+              console.log(keyBedroom)
+              for(const keyBed in object[key][keyBedroom]){
+                  console.log(keyBed)
+              }
+
+          }
+      }
+  }
+  
+  getkey(apartment)
+  
+  /**
+   * Kết quả mong muốn:
+   * bedroom
+   * area
+   * bed
+   * type
+   * price
+   * Chú ý: không cần hiển thị ra đúng thứ tự như trên
+   */
+  
     
 
