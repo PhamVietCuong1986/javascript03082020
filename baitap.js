0d352738fcf21db43008cc36dbc6eddf1b0e43f5
Nguyen van D <vietcuong1226@gmail.com>
2020-08-10 14:53:50 +0700

    bai tap

---
+++ baitap.js
@@ -0,0 +1,24 @@
+//bai tap 1
+//2 – Kiểu dữ liệu
+/**
+ * Sử dụng các kiến thức đã học trong bài khai báo các biến mô tả một người, vật nào đó mà bạn thích
+ * Ví dụ dưới đây mô tả thời tiết ngày hôm nay
+ *  var today = 'Sunday';
+ *  var isCloudy = true;
+ *  var currentTemperature = 30;
+ var today = 'Sunday';
+*/
+// var today = 'sunday';
+// var isCloudy = true;
+// var currentTemperature = 30;
+// var person = {name : 'Pham tan phat',
+// age : 20,
+// gender : female
+// }
+// console.log(person.name)
+// var animal = { turkey : {name : "khi",
+// age : 2}, elephant : {name = "voi",
+// age : 10}}
+// console.log(animal.elephant)
+var a = 5
+console.log(a)
