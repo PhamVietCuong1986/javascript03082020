b33d737ed0687168b76209ba69db7ed059070258
Nguyen van D <vietcuong1226@gmail.com>
2020-08-11 15:02:15 +0700

    cac phuong thuc kieu dang dinh san trong JS

--- know2.js
+++ know2.js
@@ -37,15 +37,43 @@ C1: OBJECT. hoac OBJECT[""]
 
 //co vai kieu gia tri co the tuong tac voi chuong trinh JS (array va function). No duoc coi la kieu thu cap phien ban dac biet cua OBJECT
 
-//3. MANG ARRAY 
+//3. MANG ARRAY la mot OBJECT dac biet
+//No co the co thuoc tinh, bao gom thuoc tinh length tu dong cap nhat
 
 /* arr = [a,b,c...,,]
 truy cap dua vao chi so INDEX 0,1,2,3...
 arr[0];
 arr[1];
 */
- var arr = ["hello world", 42, true];
- console.log(arr[0]);
+//  var arr = ["hello world", 42, true];
+//  console.log(arr[0]);
+//  console.log(arr[2]);
+// console.log(arr.length);
+// console.log(typeof arr);
+
+//4. ham function
+/* la mot kieu dang OBJECT con dac biet
+*/
+// function foo(){
+//     return 42;
+// }
+// foo.bar = "hello world";
+// console.log(foo.bar);
+// console.log(foo());
+// console.log(foo);
+
+// CAC PHUONG THUC KIEU DANG DINH SAN
+ var a = "hello world";
+ var b = 3.14259;
+ console.log(a.length);
+ console.log(a.toUpperCase);
+ console.log(b.toFixed(4));
  
+
+
+
+
+
+
 
 
