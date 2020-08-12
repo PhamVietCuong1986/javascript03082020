2a01895c9a04778cf55d4206faab4210b88c868d
Nguyen van D <vietcuong1226@gmail.com>
2020-08-12 11:29:07 +0700

    chapter 3 -series 2: Function & block scope

--- scope.js
+++ scope.js
@@ -1,6 +1,6 @@
 // SCOPE & CLOSURE 
 
-// I. SCOPE
+// CHAPTER I. SCOPE
 /* XAc dinh cac bien luu tru o dau trong chuong trinh
 1. Doi voi ngon ngu truyen thong ma nguon se thong qua 3 buoc truoc khi thuc thi chuong trinh goi la bien dich
  - Tokenizing/lexing (the xac thuc) (var a = 2;)
@@ -9,9 +9,31 @@
  - Phan tich cu phap (PARSING): Lay mot mang (array) TOKEN chuyen no thanh cac phan tu cay long nhau
  dai dien cho cu phap chuong trinh. cay AST (Abstract syntax tree)
 - Cay cua (var a = 2;)
- + Nut cao nhat Variabledeclaration (gia tri 1 - value 1)
+ + Nut cao nhat Variabledeclaration (gia tri a - value a)
  + Nut con Identifier
  + Nut con khac AssegmentExpression co 1 nut con
    ++  Numberial Literal (gia tri 2 - value 2)
+- Xu ly ma CODE (CODE GENERATION)
+la qua trinh lay cay AST bien no thanh ma thuc thi
+Phan nay khac nhay rat nhieu tuy thuoc vao ngon ngu lap trinh va nen tang ma no nham den
 */
+//JAVASCRIPT cung tuan thu 3 buoc tren
+// Bat ky doan ma CODE nao cung duoc bien dich truoc khi no thuc thi
+
+//. VAI TRO CAC VAI DIEN TRONG CHUONG TRINH JS
+/* 2.1 Engine viet code
+2.2 Compiler: Phan tich cu phap va xu ly ma
+2.3 SCOPE Tap hop vaf duy tri cac dinh danh da khai bao (bien) thuc hien 
+tap hop cac quy tac nghiem ngat ve cach thuc truy cap cac ma vua thuc thi
+*/
+
+// SCOPE long nhau
+
+// CHAPTER2 scope _LEXICAL SCOPE
+ /* dua tren bien va khoi pham vi do ban tao ra tai thoi diem viet CODE
+ // Khong nen dung EVAL(), WITH() de thay doi pham vi cua bien trong scope, no lam code chay cham
+
+ 
+/* CHAPTER 3 _ SERIES 2: FUNCTION & BLOCK SCOPE
+
 
