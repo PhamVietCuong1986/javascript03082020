3019e3bb02a73239ce2779177adc44ba453c3bf5
Nguyen van D <vietcuong1226@gmail.com>
2020-08-13 16:44:22 +0700

    ham in so

--- buoi3.js
+++ buoi3.js
@@ -15,11 +15,61 @@ function nhan2(number){
 // console.log(array);
 
 // su dung callback 
-function xulymang(array , callback){// array, callback chi la tham so tuong trung cua ham xulymang
-    for(i = 0; i < array.length; i++){
-        array[i] = callback(array[i]);
+// function xulymang(array , callback){// array, callback chi la tham so tuong trung cua ham xulymang
+//     for(i = 0; i < array.length; i++){
+//         array[i] = callback(array[i]);
+//     }
+// }
+// const array1 =[1,2,3,4];// co the thay doi gia tri cac phan tu index trong mang, object cho du gan la const
+// xulymang(array1,nhan2);//gan tham so array1 =array, nhan2 = callback cho ham xulymang
+// console.log(array1);
+
+// viet cac funtion chay 1-100
+// viet funtion inSoLe
+// viet funtion inSoChan
+// viet funtion inSoChia3Du1
+// viet funtion inSoChinhPhuong
+
+function data(){
+    for(var i =1; i<=100; i++){
+    console.log(i);}
+}
+data();
+
+function inSoChan(){
+    for(var i =1; i<=100; i++){
+        if(i % 2 ===0){
+            console.log(i);
+        }
     }
 }
-const array1 =[1,2,3,4];
-xulymang(array1,nhan2);//gan tham so array1 =array, nhan2 = callback cho ham xulymang
-console.log(array1);
+ inSoChan();
+
+ function inSoLe(){
+    for(var i =1; i<=100; i++){
+        if(i % 2 ===1){
+            console.log(i);
+        }
+    }
+}
+ inSoLe();
+
+
+ function inSoChia3Du1(){
+    for(var i =1; i<=100; i++){
+        if(i % 3 ===1){
+            console.log(i);
+        }
+    }
+}
+ inSoChia3Du1();
+
+ function inSoChinhPhuong(){
+    for(var i =1; i<=100; i++){
+        if(Math.sqrt(i) % 1 ===0){
+            console.log(i);
+        }
+    }
+}
+ inSoChinhPhuong();
+ 
