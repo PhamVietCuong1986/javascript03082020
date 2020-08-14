c47259a984ef3acded23a11ae726887e3dab4a7d
Nguyen van D <vietcuong1226@gmail.com>
2020-08-14 21:22:23 +0700

    vi du ve mang la object su dung ham REDUCE de gop

--- buoi4.js
+++ buoi4.js
@@ -1,9 +1,34 @@
 // Ham REDUCE Gop phan tu
 // arr.reduce(callback[, initialValue])
- const arrayNums = [1,2,3,4,5,6,7,8,9,10];// yeu cau su dung ham REDUCE tinh tong cac phan tu cua mang
- const total = arrayNums.reduce(function(tichLuy , currentValue){
- console.log(tichLuy);// total la tham so tich luy
- return tichLuy + currentValue;// tham so tichLuy ~~ voi total trong video
- },0)// 0 la gia tri tich luy ban dau cua total nen se co gia tri la 0
+//  const arrayNums = [1,2,3,4,5,6,7,8,9,10];// yeu cau su dung ham REDUCE tinh tong cac phan tu cua mang
+//  const total = arrayNums.reduce(function(tichLuy , currentValue){
+//  console.log(tichLuy);// total la tham so tich luy
+//  return tichLuy + currentValue;// tham so tichLuy ~~ voi total trong video
+//  },0)// 0 la gia tri tich luy ban dau cua total nen se co gia tri la 0
 // do mang co 10 phan tu nen se ung 10 vong lap vaf in ra tuong ung 10 ket qua tich luy total
+
+// vi du 
+// Trả về số lượng người đã vote
+function totalVotes(arr) {
+    var total =  arr.reduce(function(acc,voter){
+    return acc += voter.voted;
+},0)
+console.log(total);
+}
+
+var voters = [
+  {name:'Bob' , age: 30, voted: true},
+  {name:'Jake' , age: 32, voted: true},
+  {name:'Kate' , age: 25, voted: false},
+  {name:'Sam' , age: 20, voted: false},
+  {name:'Phil' , age: 21, voted: true},
+  {name:'Ed' , age:55, voted:true},
+  {name:'Tami' , age: 54, voted:true},
+  {name: 'Mary', age: 31, voted: false},
+  {name: 'Becky', age: 43, voted: false},
+  {name: 'Joey', age: 41, voted: true},
+  {name: 'Jeff', age: 30, voted: true},
+  {name: 'Zack', age: 19, voted: false}
+];
+totalVotes(voters) // 7// gan tham so arr =voters
 
